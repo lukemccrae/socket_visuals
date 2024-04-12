6d906767edaea26af9ef68f5826363c9aa6defe7
lukemccrae <lukemccrae@gmail.com>
2024-04-12 13:52:37 -0600

    log

--- src/App.tsx
+++ src/App.tsx
@@ -67,6 +67,10 @@ function App() {
   }
 
   const colors = ["blue", "green", "yellow", "purple"];
+  console.log(
+    process.env.REACT_APP_WS_X_API_KEY,
+    "<< process.env.REACT_APP_WS_X_API_KEY"
+  );
 
   return (
     <div>
