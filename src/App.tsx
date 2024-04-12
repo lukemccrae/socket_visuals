390b9153cec31394f9d60a6727127c44658b9648
lukemccrae <lukemccrae@gmail.com>
2024-04-12 13:39:59 -0600

    add xapikey env

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ Amplify.configure({
         "https://afno5ipvkfamfagfok7ad4qnwm.appsync-api.us-west-1.amazonaws.com/graphql",
       region: "us-west-1",
       defaultAuthMode: "apiKey",
-      apiKey: "da2-7fj75oc5ufginkjobizl45jy6m",
+      apiKey: process.env.WS_X_API_KEY,
     },
   },
 });
@@ -66,28 +66,32 @@ function App() {
     data = received;
   }
 
-  function sketch(p5: P5CanvasInstance) {
-    p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL);
-
-    p5.draw = () => {
-      p5.background(250);
-      p5.normalMaterial();
-      p5.push();
-      p5.rotateZ(p5.frameCount * 0.01);
-      p5.rotateX(0.05);
-      p5.rotateY(p5.frameCount * 0.01);
-      p5.plane(100);
-      p5.pop();
-    };
-  }
+  const colors = ["blue", "green", "yellow", "purple"];
 
   return (
     <div>
       <h5>channel: {data.name}</h5>
       <div>note: {data.midi.note}</div>
       <div>value: {data.midi.value}</div>
-      <ReactP5Wrapper sketch={sketch} data={data} />
-      <Mouth rows={20} columns={20} circleSize={10}></Mouth>
+      <div
+        style={{
+          height: "100%",
+          margin: "0",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            width: "300px",
+            height: "300px",
+            backgroundColor: colors[data.midi.value],
+            textAlign: "center",
+            lineHeight: "200px",
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
